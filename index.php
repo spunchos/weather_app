9705678c2b50a785cf2f90bcd1707f6a8c5dda50
spunchos <spunchosss@gmail.com>
2022-02-22 22:39:59 +0600

    final version of weather app

--- index.php
+++ index.php
@@ -1,59 +1,90 @@
 <?php
 
-$weather = '';
+  error_reporting(E_ERROR | E_PARSE);
+
+  $weather = "";
+
+  $error = "";
 
   if(isset($_GET['city'])){
-    $pogoda = file_get_contents('https://api.openweathermap.org/data/2.5/weather?q='.$_GET['city'].'&appid=771eb3ad976b6d9672bca4c8194f006b');
 
-    $gavno = json_decode($pogoda, true);
+    $urlContent = file_get_contents('https://api.openweathermap.org/data/2.5/weather?q='.$_GET['city'].'&units=metric&appid=771eb3ad976b6d9672bca4c8194f006b');
 
-    $weather = $gavno['main']['temp'];
- 
+    $forcastArray = json_decode($urlContent, true);
+
+    
+
+    if($forcastArray['cod'] == 200) {
+
+      $weather = 'The weather in '.$_GET['city'].' is '.$forcastArray['weather'][0]['description'];
+
+      $weather = $weather.'. The temperature is '.$forcastArray['main']['temp'].'&#8451;'.'. The speed of wind is '.$forcastArray['wind']['speed'].' m/sec';
+
+    } else {
+      $error = "The city name is incorrect, please try another name";
+    }
+
+    
+
   }
+
 ?>
+
 
 <!doctype html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+    <link rel="stylesheet" type="text/css" href="styles/style.css">
 
-
-    <title>pogoda</title>
+    <title>Weather App</title>
   </head>
   <body>
-    <div class="container" id="main">
-      <h1>gorod tvoy pishi</h1>
+    
+
+    <div class="container" id="mainDiv">
+      <h1>Weather In The City</h1>
+
       <form>
-        <div class="align-items-center">
-        <div class="col-auto">
-          <label for="city" class="col-form-label">city</label>
+        <div class="form-group">
+          <label for="city">Input a city name</label>
+          <input class="form-control" id="city" name="city" aria-describedby="Forcast city" placeholder="Enter city name">
         </div>
-        <div class="col-auto">
-          <input placeholder="gorod pishi" type="text" id="city" name="city" class="form-control" aria-describedby="city">
-        </div>
-        <div class="col-auto"></div>
-        <button type="submit" class="btn btn-primary">Показать</button>
-        </div>
+        
+        <button type="submit" class="btn btn-primary">Submit</button>
       </form>
-        <div id="asd">
+    </div>
+
+    <div id="forecastDiv">
+      
+      
+
         <?php
 
-        if ($weather) {
-          echo '<div class="alert alert-success" role="alert">'.$weather.'</div>';
-        }
+          if($weather){
+
+              echo '<div class="alert alert-primary" role="alert">'.$weather.'</div>';
+
+          } else if($error) {
+              echo '<div class="alert alert-danger" role="alert">'.$error.'</div>';
+          }
+
+          ?>
         
+      
 
-        ?>
-        </div>
-      </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
+    </div>
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
 </html>
